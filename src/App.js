0d614161de961814da24d0220f95162927c7486b
natashazelinka <natashazelinka@googlemail.com>
2023-12-24 13:51:17 +0000

    Updated css

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <footer>
-          Coded by Natasha Zelinka,{" "}
+          Coded by Natasha Zelinka{" "}
           <a
             href="https://github.com/natashazelinka/weather-react"
-            target="_blank"
             rel="noreferrer"
           >
-            open-source code
+            open-source code on GitHub 👩‍💻
           </a>{" "}
-          on GitHub.
         </footer>
       </div>
     </div>
