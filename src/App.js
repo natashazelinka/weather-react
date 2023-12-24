05c0775c5a3694766d1691465bc758b62a417724
natashazelinka <natashazelinka@googlemail.com>
2023-12-24 13:26:05 +0000

    Added axios intergration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World</h1>
+        <Weather />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
