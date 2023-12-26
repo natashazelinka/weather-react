13ff1e674c2d0fa2a816f86702b6150515c1613f
natashazelinka <natashazelinka@googlemail.com>
2023-12-26 10:19:54 +0000

    New deloy with lines 5-13 changes App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
-    <footer>
-      Coded by Natasha Zelinka,{" "}
-      <a
-        href="https://github.com/natashazelinka/weather-react"
-        target="_blank"
-        rel="noreferrer"
-      >
-        open-source code
-      </a>{" "}
-      on GitHub.
-    </footer>
+    <div className="App">
+      <div className="container">
+        <br />
+        <footer>
+          This project is built with{" "}
+          <a
+            href="https://github.com/Shauna845/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>{" "}
+          created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/65141-shauna-bratten"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Shauna Bratten{" "}
+          </a>
+        </footer>
+      </div>
+    </div>
   );
 }
