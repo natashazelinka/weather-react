0b4835f795517bf308b3b76015b37a2896b03fe7
natashazelinka <natashazelinka@googlemail.com>
2023-12-30 19:03:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import "./App.css";
+import Weather from "./Weather";
 
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        <Weather />
+        <div>
+          <footer>
+            <a href="https://github.com/MariJ12/weather-mariapp-react">
+              Github
+            </a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
